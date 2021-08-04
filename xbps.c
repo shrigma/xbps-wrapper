26f17dcfd297d8916ffae84df7a6a87a8aa307bb
shrigma <88425188+shrigma@users.noreply.github.com>
2021-08-04 13:32:22 +0000

    Update xbps.c
    update to xbps

--- xbps.c
+++ xbps.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+//this is terrible code with way too many if statements
 int main(int argc, char* argv[])
 {
-
-        const static char helptext[] = "xbps [options] [program]\n"
+				const static char helptext[] = "xbps [options] [program]\n"
         "-h for help (this screen)\n"
         "-i for installation mode\n"
         "-r for removal mode\n"
@@ -15,41 +15,38 @@ int main(int argc, char* argv[])
                         fprintf(stdout, "%s", helptext);
                         return 0;
         }
-        else
+        char prog[30];
+        /* this checks to see which options are used */
+        if(strcmp(argv[1], "-h") == 0)
         {
-                char prog[30];
-                /* this checks to see which options are used */
-                if(strcmp(argv[1], "-h") == 0)
-                {
-                        fprintf(stdout, "%s", helptext);
-                        return 0;
-                }
-                /* mode options */
-                else if (strcmp(argv[1], "-i") == 0)
-                {
-                        strcpy(prog, "/bin/xbps-install");
-                }
-                else if (strcmp(argv[1], "-r") == 0)
-                {
-                        strcpy(prog, "/bin/xbps-remove");
-                }
-                else if (strcmp(argv[1], "-q") == 0)
-                {
-                        strcpy(prog, "/bin/xbps-query");
-                }
-                /* this grabs the options/packages */
-                char* pkgs[100];
-                int ltime = 0;
-                for(int i = 1; i < argc; i++)
-                {
-                        pkgs[ltime] = argv[i];
-                        ltime += 1;
-                }
-                pkgs[ltime] = NULL;
-                /* this executes the appropriate xbps command
-                 * xbps -i -Su becomes
-                 * xbps-install -Su*/
-                execv(prog, pkgs);
+                fprintf(stdout, "%s", helptext);
+                return 0;
         }
+        /* mode options */
+        else if (strcmp(argv[1], "-i") == 0)
+        {
+                strcpy(prog, "/bin/xbps-install");
+        }
+        else if (strcmp(argv[1], "-r") == 0)
+        {
+                strcpy(prog, "/bin/xbps-remove");
+        }
+        else if (strcmp(argv[1], "-q") == 0)
+        {
+                strcpy(prog, "/bin/xbps-query");
+        }
+        /* this grabs the options/packages */
+        char* pkgs[100];
+        int ltime = 0;
+        for(int i = 1; i < argc; i++)
+        {
+                pkgs[ltime] = argv[i];
+                ltime += 1;
+        }
+        pkgs[ltime] = NULL;
+        /* this executes the appropriate xbps command
+         * xbps -i -Su becomes
+         * xbps-install -Su*/
+        execv(prog, pkgs);
         return 0;
 }
